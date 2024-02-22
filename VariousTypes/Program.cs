a88fd00ee9790c7f28715e64d0f5d4b4197834e1
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-22 21:04:53 +0000

    Variaveis tipo inteiro

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var_int = 1;
+            Console.WriteLine($"variável int = {var_int}");
+            uint var_uint = 2U;
+            Console.WriteLine($"variável uint = {var_uint}");
+            long var_long = 3L;
+            Console.WriteLine($"variável long = {var_long}");
+            ulong var_ulong = 4UL;
+            Console.WriteLine($"variável ulong = {var_ulong}");
+            byte var_byte = 5;
+            Console.WriteLine($"variável byte = {var_byte}");
+            sbyte var_sbyte = 6;
+            Console.WriteLine($"variável sbyte = {var_sbyte}");
+            short var_short = 7;
+            Console.WriteLine($"variável short = {var_short}");
+            ushort var_ushort = 8;
+            Console.WriteLine($"variável ushort = {var_ushort}");
+            char var_char = '9';
+            Console.WriteLine($"variável char = {var_char}");
+        }
+    }
+}
