20adf288da7240d2990825b23d1c12671037bb75
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-22 19:17:05 +0000

    Bye world

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
-
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Primeira aula de LP1
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Programa que mostra no ecrã duas frases
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
