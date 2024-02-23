52ae6b16fdb459237a5665dfac1aea8171eebf98
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-23 10:45:57 +0000

    Primeiro teste

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -4,9 +4,22 @@ namespace AskForInput
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite um número inteiro: ");
+            string resposta = Console.ReadLine();
+
+            int transfoma = int.Parse(resposta);
+
+            Console.Write("Digite um número real: ");
+            string resposta2 = Console.ReadLine();
+            float floating = int.Parse(resposta2);
+
+            Console.WriteLine($"{resposta}+{resposta2}={transfoma + floating}");
         }
     }
 }
