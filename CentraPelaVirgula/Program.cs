4c5185d8c7c94e5092452157d8ca9e064fb99626
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-23 14:02:58 +0000

    Ex Extra finalizado

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -6,7 +6,17 @@ namespace CentraPelaVirgula
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insere 4 números reais:");
+            string resposta1 = Console.ReadLine(); //Pede 1 numero real
+            float n1 = float.Parse(resposta1); //muda essa string para um float
+            string resposta2 = Console.ReadLine(); //Pede 1 numero real
+            float n2 = float.Parse(resposta2); //muda essa string para um float
+            string resposta3 = Console.ReadLine(); //Pede 1 numero real
+            float n3 = float.Parse(resposta3); //muda essa string para um float
+            string resposta4 = Console.ReadLine(); //Pede 1 numero real
+            float n4 = float.Parse(resposta4); //muda essa string para um float
+
+            Console.WriteLine($"{n1,6:f2}\n{n2,6:f2}\n{n3,6:f2}\n{n4,6:f2}");
         }
     }
 }
