08b09db00b322c19053aa5d9d5276094dc334952
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-23 14:11:49 +0000

    Adicionado xml e um comentário por cima do 'print'

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -4,6 +4,11 @@ namespace CentraPelaVirgula
 {
     class Program
     {
+        /// <summary>
+        /// Programa que pede ao utilizador 4 números reais e os centra à
+        /// direita com o máximo de 6 caracteres e com 2 casa decimais
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Insere 4 números reais:");
@@ -15,7 +20,9 @@ namespace CentraPelaVirgula
             float n3 = float.Parse(resposta3); //muda essa string para um float
             string resposta4 = Console.ReadLine(); //Pede 1 numero real
             float n4 = float.Parse(resposta4); //muda essa string para um float
-
+            
+            //Imprime todos os floats com 2 casas decimais e encostados à
+            //direita com 6 caracteres
             Console.WriteLine($"{n1,6:f2}\n{n2,6:f2}\n{n3,6:f2}\n{n4,6:f2}");
         }
     }
